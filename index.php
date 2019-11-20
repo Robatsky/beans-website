8ce9699c7d83561f02904979f06c7f338cea58a1
Ruben Eekhof <45236226+RubenJ01@users.noreply.github.com>
2019-11-20 15:37:18 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Jungbin's art portofolio website.
+@author Ruben Eekhof
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Beans website</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="top-section">
+	<p id="header">J U N G B I N ' s  A R T  P O R T F O L I O</p>
+</div>
+<div class="main-section">
+	<div class="navbar">
+		<input type="text" name="Searchbar" placeholder="Search!">
+		<a href="#">About Me</a>
+		<a href="#">Other Art</a>
+		<a href="#">Digital Art</a>
+		<a href="#">Faq</a>
+	</div>
+	<div class="sidebar">
+		<div class="profilepicture">
+			<img src="art/profilepicture.jpg">
+		</div>
+		<div class="status">
+			<textarea readonly>Current thoughts and status.</textarea>
+		</div>
+		<div class="fav_songs">
+			<p>Favourite songs</p>
+			<iframe src="https://open.spotify.com/embed/playlist/6Bk4iUyRdMNkWlukwGq1TM" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+		</div>
+	</div>
+	<div class="hexagons">
+		<div class="hexagon-gallery">
+			<div class="hex"><img src="art/namijhinfinal.jpg"></div>
+		    <div class="hex"><img src="art/namijhinfinal.jpg"></div>
+		    <div class="hex"><img src="art/namijhinfinal.jpg"></div>
+		    <div class="hex"><img src="art/namijhinfinal.jpg"></div>
+		    <div class="hex"><img src="art/namijhinfinal.jpg"></div>
+		    <div class="hex"><img src="art/namijhinfinal.jpg"></div>
+		    <div class="hex"><img src="art/namijhinfinal.jpg"></div>
+		    <div class="hex"><img src="art/namijhinfinal.jpg"></div>
+		    <div class="hex"><img src="art/namijhinfinal.jpg"></div>
+		    <div class="hex"><img src="art/namijhinfinal.jpg"></div>
+		</div>
+</div>
+</div>
+</body>
+</html>
